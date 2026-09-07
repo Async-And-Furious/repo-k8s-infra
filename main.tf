41b9680b0f03a7607d0337613893f3f03ad980ce
Jorge Lucas <jorgelucastcoronado@gmail.com>
2026-09-07 16:42:08 -0300

    fix: use API liveness for ALB health checks

--- main.tf
+++ main.tf
@@ -55,6 +55,7 @@ module "internal_alb" {
   environment        = var.environment
   vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
+  health_check_path  = "/api/v1/health/live"
 }
 
 provider "helm" {
